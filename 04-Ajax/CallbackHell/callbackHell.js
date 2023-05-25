3eea94676cce6812cf18c614d62d5fe2fb2a745b
Adnan Khan <adnan.khan2@kyndryl.com>
2023-05-25 23:07:25 +0500

    Discuss promises

--- 04-Ajax/CallbackHell/callbackHell.js
+++ 04-Ajax/CallbackHell/callbackHell.js
@@ -1,10 +1,10 @@
-function getData(endpoint) {
+function getData(endpoint, cb) {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", endpoint);
 
   xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      console.log(JSON.parse(this.responseText));
+      cb(JSON.parse(this.responseText));
     }
   };
 
@@ -13,6 +13,20 @@ function getData(endpoint) {
   }, Math.floor(Math.random() * 3000) + 1000);
 }
 
-getData("movies.json");
-getData("actors.json");
-getData("directors.json");
+// getData("movies.json");
+// getData("actors.json");
+// getData("directors.json");
+
+getData("./movies.json", (data) => {
+  console.log(data);
+
+  getData("./actors.json", (data) => {
+    console.log(data);
+
+    getData("./directors.json", (data) => {
+      console.log(data);
+    });
+  });
+});
+
+// * So you can see the issue here. We have nested 3 callback functions within each other. This is called callback hell. It is not very readable and it can get very messy very quickly. However it does work, it gets the data in the correct order.
